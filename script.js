a756d0cfe9b626ba4538536af19c53350c9a64c3
Puroo Kulshrestha <purookulshrestha012@gmail.com>
2024-07-17 13:14:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let speech= new SpeechSynthesisUtterance();
+let voice= [];
+let voiceSelect= document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged= ()=>{
+    voice= window.speechSynthesis.getVoices();
+    speech.voice= voice[0];
+    voice.forEach((voice,i)=>(voiceSelect.options[i]= new Option(voice.name,i)));
+};
+
+voiceSelect.addEventListener("change",()=>{
+    speech.voice= voice[voiceSelect.value];
+});
+
+document.querySelector("#speak-btn").addEventListener("click",()=>{
+    speech.text= document.querySelector("textarea").value;
+    window.speechSynthesis.speak(speech);
+});
